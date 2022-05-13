b9e10d2e97706d7181d2b54663acb27828d86198
Alex Zhao <axyzhao@berkeley.edu>
2022-05-13 13:37:35 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import one from './Images/1.jpg';
+import two from './Images/2.jpg';
+import three from './Images/3.jpg';
+import four from './Images/4.jpg';
+import five from './Images/5.jpg';
+import six from './Images/6.jpg';
+import seven from './Images/7.jpg';
+import eight from './Images/8.jpg';
 
-function App() {
+function App(props)
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Carousel showArrows={true}>
+          <div>
+              <img src={one} />
+              <p className="legend">Legend 1</p>
+          </div>
+          <div>
+              <img src={two} />
+              <p className="legend">Legend 2</p>
+          </div>
+          <div>
+              <img src={three} />
+              <p className="legend">Legend 3</p>
+          </div>
+      </Carousel>
   );
 }
-
 export default App;
