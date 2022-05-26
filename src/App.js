87762e1001bc3c36a4eb4b0372fecc6ef8250cc5
Alex Zhao <axyzhao@berkeley.edu>
2022-05-26 22:17:47 +0800

    wip draft

--- src/App.js
+++ src/App.js
@@ -13,26 +13,28 @@ import jail from './Audio/jail.mp4';
 import jail_pic from './Audio/jail.png';
 
 import waterfall from './Audio/waterfall.wav';
-import waterfall_pic from './Audio/thesis.png';
+import waterfall_pic from './Audio/waterfall.png';
 
 import boba from './Audio/boba.wav';
-import boba_pic from './Audio/bike.png';
+import boba_pic from './Audio/boba.png';
 
 import manzou from './Audio/manzou.wav';
-import manzou_pic from './Audio/bike.png';
+import manzou_pic from './Audio/boba.png';
 
 import thesis from './Audio/thesis.mp4';
 import thesis_pic from './Audio/thesis.png';
 
 import singer from './Audio/singer.wav';
-import singer_pic from './Audio/bike.png';
+import singer_pic from './Audio/singer.png';
 
 import bike from './Audio/bike.mp4';
 import bike_pic from './Audio/bike.png';
 
 import temp from './Audio/temperature.m4a';
+import temp_pic from './Audio/temperature.png';
 
 import hello from './Audio/hello.m4a';
+import hello_pic from './Audio/hello.png';
 
 import Video_Card from './components/Cards/Video_Card';
 import Title_Card from './components/Cards/Title_Card';
@@ -44,7 +46,7 @@ function App(props)
 {
 
   const images = [jail_pic, waterfall_pic, boba_pic,
-    manzou_pic, thesis_pic, singer_pic, bike_pic, bike_pic, bike_pic];
+    manzou_pic, thesis_pic, singer_pic, bike_pic, hello_pic, temp_pic];
   const sounds = [jail, waterfall, boba,
       manzou, thesis, singer, bike, hello, temp];
   return (
@@ -73,7 +75,7 @@ function App(props)
           <Sound_Card
           images={images}
           sounds={sounds}
-          caption="Sounds of Beijing > Arjun going off on drums"
+          caption="Sounds of Beijing > Select your sound on Tsinghua"
           />
       </Carousel>
   );
