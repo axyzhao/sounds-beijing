9b16d422029937324a35c6fd435408c73f8933fc
Alex Zhao <axyzhao@berkeley.edu>
2022-06-08 22:48:39 +0800

    rebuild

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-#!/bin/#!/usr/bin/env bash
- #rm -r docs && 
- npm run build && mv build docs && git add . && git commit -m "rebuild" && git push
+#!/bin/bash
+ rm -r docs && npm run build && mv build docs && git add . && git commit -m "rebuild" && git push
